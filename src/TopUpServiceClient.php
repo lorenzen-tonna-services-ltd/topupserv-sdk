12f7dbe76ad65274af41ead11c3f3271fb9e6e97
j_lorenzen <jannik.lorenzen@modix.eu>
2019-04-15 18:32:07 +0100

    initial commit

---
+++ src/TopUpServiceClient.php
@@ -0,0 +1,130 @@
+<?php
+namespace TopUpService;
+
+use TopUpService\Entity\TopUp;
+
+class TopUpServiceClient
+{
+	/**
+	 * @var string
+	 */
+	private $apiKey;
+
+	/**
+	 * @var string
+	 */
+	private $apiSecret;
+
+	/**
+	 * @var integer
+	 */
+	private $apiClientId;
+
+
+	public function __construct(array $configuration)
+	{
+		$this->apiSecret   = $configuration['secret'];
+		$this->apiKey      = $configuration['key'];
+		$this->apiClientId = $configuration['client'];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getApiKey()
+	{
+		return $this->apiKey;
+	}
+
+	/**
+	 * @param string $apiKey
+	 */
+	public function setApiKey($apiKey)
+	{
+		$this->apiKey = $apiKey;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getApiSecret()
+	{
+		return $this->apiSecret;
+	}
+
+	/**
+	 * @param string $apiSecret
+	 */
+	public function setApiSecret($apiSecret)
+	{
+		$this->apiSecret = $apiSecret;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getApiClientId()
+	{
+		return $this->apiClientId;
+	}
+
+	/**
+	 * @param int $apiClientId
+	 */
+	public function setApiClientId($apiClientId)
+	{
+		$this->apiClientId = $apiClientId;
+	}
+
+	/**
+	 * @param TopUp $topUp
+	 * @return integer
+	 */
+	public function create(TopUp $topUp)
+	{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL,"http://api.topupserv.lorenzentonna.com/topup");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, [
+			'provider'     => $topUp->getProvider(),
+			'phone'        => $topUp->getPhone(),
+			'amount'       => $topUp->getAmount(),
+			'currency'     => $topUp->getCurrency(),
+			'operator'     => $topUp->getOperator(),
+			'reference'    => $topUp->getReference(),
+			'callback_url' => $topUp->getCallbackUrl(),
+			'date'         => $topUp->getDate()
+		]);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$headers = [
+			'X-Api-Key: '. $this->getApiKey(),
+			'X-Api-Secret: '. $this->getApiSecret(),
+			'X-Api-Client-Id: '. $this->getApiClientId()
+		];
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		$data = json_decode($response, true);
+
+		return $data['id'];
+	}
+
+	/**
+	 * @param integer $topUpId
+	 */
+	public function delete($topUpId)
+	{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "http://api.topupserv.lorenzentonna.com/topup?id=". $topUpId);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+		curl_exec($ch);
+		curl_close($ch);
+	}
+}
---
+++ src/entity/TopUp.php
@@ -0,0 +1,176 @@
+<?php
+namespace TopUpService\Entity;
+
+class TopUp
+{
+	/**
+	 * @var string
+	 */
+	private $provider;
+
+	/**
+	 * @var string
+	 */
+	private $phone;
+
+	/**
+	 * @var integer
+	 */
+	private $amount;
+
+	/**
+	 * @var string
+	 */
+	private $currency;
+
+	/**
+	 * @var string
+	 */
+	private $operator;
+
+	/**
+	 * @var string
+	 */
+	private $reference;
+
+	/**
+	 * @var string
+	 */
+	private $callback_url;
+
+	/**
+	 * @var \DateTime
+	 */
+	private $date;
+
+	/**
+	 * @return string
+	 */
+	public function getProvider()
+	{
+		return $this->provider;
+	}
+
+	/**
+	 * @param string $provider
+	 */
+	public function setProvider($provider)
+	{
+		$this->provider = $provider;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPhone()
+	{
+		return $this->phone;
+	}
+
+	/**
+	 * @param string $phone
+	 */
+	public function setPhone($phone)
+	{
+		$this->phone = $phone;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getAmount()
+	{
+		return $this->amount;
+	}
+
+	/**
+	 * @param int $amount
+	 */
+	public function setAmount($amount)
+	{
+		$this->amount = $amount;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCurrency()
+	{
+		return $this->currency;
+	}
+
+	/**
+	 * @param string $currency
+	 */
+	public function setCurrency($currency)
+	{
+		$this->currency = $currency;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getOperator()
+	{
+		return $this->operator;
+	}
+
+	/**
+	 * @param string $operator
+	 */
+	public function setOperator($operator)
+	{
+		$this->operator = $operator;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getReference()
+	{
+		return $this->reference;
+	}
+
+	/**
+	 * @param string $reference
+	 */
+	public function setReference($reference)
+	{
+		$this->reference = $reference;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCallbackUrl()
+	{
+		return $this->callback_url;
+	}
+
+	/**
+	 * @param string $callback_url
+	 */
+	public function setCallbackUrl($callback_url)
+	{
+		$this->callback_url = $callback_url;
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getDate()
+	{
+		if (empty($this->date)) {
+			return null;
+		}
+		return $this->date->format('d.m.Y H:i:s');
+	}
+
+	/**
+	 * @param \DateTime $date
+	 */
+	public function setDate($date)
+	{
+		$this->date = $date;
+	}
+}
